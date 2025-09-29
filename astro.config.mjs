7e2b31da60e3cb792da8f9adf0ff95e8b4d1451e
philomagi <tooppoo@users.noreply.github.com>
2025-09-29 21:55:22 +0000

    topページ仮置き

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://philomagi.dev',
+  integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
