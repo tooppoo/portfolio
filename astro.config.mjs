e3f2a7556252aed853e0c69705cdb9157c274ed2
philomagi <tooppoo@users.noreply.github.com>
2025-09-29 22:05:56 +0000

    setup astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // SSG
   site: 'https://philomagi.dev',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    },
-
-    imageService: "cloudflare"
-  })
 });
