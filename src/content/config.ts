1f5c5a095669c12da44a1a5d210206309aaebfb9
philomagi <tooppoo@users.noreply.github.com>
2025-09-30 07:32:25 +0000

    refactor

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,40 +1,26 @@
 import { defineCollection, z } from 'astro:content';
 
-const armoredcoreCollection = defineCollection({
+const baseArticleScheme = ({ category }: { category: string }) => ({
   type: 'content',
   schema: z.object({
     title: z.string(),
     description: z.string(),
     publishDate: z.date().optional().default(() => new Date()),
-    category: z.string().default('armoredcore'),
+    category: z.literal(category),
     tags: z.array(z.string()).optional().default([]),
     layout: z.string().optional().default('../layouts/Layout.astro'),
   }),
-});
+} as const)
 
-const techArchitectCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    publishDate: z.date().optional().default(() => new Date()),
-    category: z.string().default('tech'),
-    tags: z.array(z.string()).optional().default([]),
-    layout: z.string().optional().default('../layouts/Layout.astro'),
-  }),
-});
-
-const philosophyCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    publishDate: z.date().optional().default(() => new Date()),
-    category: z.string().default('philosophy'),
-    tags: z.array(z.string()).optional().default([]),
-    layout: z.string().optional().default('../layouts/Layout.astro'),
-  }),
-});
+const armoredcoreCollection = defineCollection(baseArticleScheme({
+  category: 'armoredcore',
+}));
+const techArchitectCollection = defineCollection(baseArticleScheme({
+  category: 'tech',
+}));
+const philosophyCollection = defineCollection(baseArticleScheme({
+  category: 'philosophy',
+}));
 
 export const collections = {
   armoredcore: armoredcoreCollection,
