89fe543bafab3cc8bc91e8f406f263dce452aae7
philomagi <tooppoo@users.noreply.github.com>
2025-09-30 00:49:49 +0000

    create article page

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const armoredcoreCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date().optional().default(() => new Date()),
+    category: z.string().default('armoredcore'),
+    tags: z.array(z.string()).optional().default([]),
+    layout: z.string().optional().default('../layouts/Layout.astro'),
+  }),
+});
+
+export const collections = {
+  armoredcore: armoredcoreCollection,
+};
