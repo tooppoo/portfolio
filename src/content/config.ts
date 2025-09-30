14767a3bc8cc9d3e0fef6657674315a437c085ab
philomagi <tooppoo@users.noreply.github.com>
2025-09-30 00:59:32 +0000

    update category filter

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,19 @@ const armoredcoreCollection = defineCollection({
   }),
 });
 
+const techArchitectCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishDate: z.date().optional().default(() => new Date()),
+    category: z.string().default('tech'),
+    tags: z.array(z.string()).optional().default([]),
+    layout: z.string().optional().default('../layouts/Layout.astro'),
+  }),
+});
+
 export const collections = {
   armoredcore: armoredcoreCollection,
+  'tech': techArchitectCollection,
 };
