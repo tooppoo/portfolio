45c84ccf18d12ff309d9968ad82ae9bdbcb22d87
philomagi <tooppoo56@gmail.com>
2025-12-26 18:19:49 +0900

    install solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static', // SSG
   site: 'https://philomagi.dev',
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap()
-  ],
+  integrations: [tailwind(), mdx(), sitemap(), solidJs()],
 });
